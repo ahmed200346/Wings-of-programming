ec7c374c8a8cf32df92ad3dba7a72f024424cb13
ahmed200346 <129754857+ahmed200346@users.noreply.github.com>
2023-05-08 12:52:47 +0100

    Version finale de mini-map.h et mini_map.c

--- mini_map.c
+++ mini_map.c
@@ -5,115 +5,222 @@
 #include<SDL/SDL_image.h>
 #include<SDL/SDL_mixer.h>
 #include "mini_map.h"
+/** 
+*@file mini_map.c
+*@brief Testing program
+*@author Ahmed Aziz
+*@version 0.1
+*@date May 7 2023
+*Testing doxygen 
+*/
+/**
+*@brief Load Background du niveau 1
+*@param pointeur sur image
+*@return rien
+*/
 void level_1(image *i)
 {
-	i->image=IMG_Load("/home/aziz/level1.png");
+	i->image=IMG_Load("level1.png");
 	i->pos1.x=0;
 	i->pos1.y=0;
 }
+/**
+*@brief Load Background du niveau 2
+*@param pointeur sur image
+*@return rien
+*/
 void level_2(image *i)
 {
-	i->image=IMG_Load("/home/aziz/level2.png");
+	i->image=IMG_Load("level2.png");
 	i->pos1.x=0;
 	i->pos1.y=0;
 }
+/**
+*@brief Load Background du niveau 3
+*@param pointeur sur image
+*@return rien
+*/
 void level_3(image *i)
 {
-	i->image=IMG_Load("/home/aziz/level3.png");
+	i->image=IMG_Load("level3.png");
 	i->pos1.x=0;
 	i->pos1.y=0;
 }
-//1
+/**
+*@brief Load mini_map avec animation du niveau 1 et initialiser sa position avec la position du mini_hero
+*@param pointeur sur mini map 
+*@return rien
+*/
 void initmap_level1(mini_map *m)
 {
-	m->background=IMG_Load("/home/aziz/mini_level1.png");
-	m->posbackground.x=400;
+	m->background=IMG_Load("mini_level1.png");
+	m->posbackground.x=1000;
 	m->posbackground.y=0;
-	m->mini_personne=IMG_Load("/home/aziz/sprite0.png");
-	m->posmini_perosnne.x=450;
-	m->posmini_perosnne.y=100;
+	m->mini_personne=IMG_Load("gab.png");
+	m->posmini_perosnne.x=1050;
+	m->posmini_perosnne.y=110;
 }
+/**
+*@brief Load mini_map  avec animation du niveau 2 et initialiser sa position avec la position du mini_hero
+*@param pointeur sur mini map 
+*@return rien
+*/
 void initmap_level2(mini_map *m)
 {
-	m->background=IMG_Load("/home/aziz/mini_level2.png");
-	m->posbackground.x=400;
+	m->background=IMG_Load("mini_level2.png");
+	m->posbackground.x=1000;
 	m->posbackground.y=0;
-	m->mini_personne=IMG_Load("/home/aziz/sprite0.png");
-	m->posmini_perosnne.x=450;
-	m->posmini_perosnne.y=100;
+	m->mini_personne=IMG_Load("gab.png");
+	m->posmini_perosnne.x=1030;
+	m->posmini_perosnne.y=132;
 }
+/**
+*@brief Load mini_map avec animation du niveau 3 et initialiser sa position avec la position du mini_hero
+*@param pointeur sur mini map 
+*@return rien
+*/
 void initmap_level3(mini_map *m)
 {
-	m->background=IMG_Load("/home/aziz/mini_level3.png");
+	m->background=IMG_Load("mini_level3.png");
 	m->posbackground.x=400;
 	m->posbackground.y=0;
-	m->mini_personne=IMG_Load("/home/aziz/sprite0.png");
-	m->posmini_perosnne.x=450;
-	m->posmini_perosnne.y=100;
+	m->mini_personne=IMG_Load("gab.png");
+	m->posmini_perosnne.x=1030;
+	m->posmini_perosnne.y=125;
 }
-//2 Afficher mini_map et la version petite du captain  
+/**
+*@brief Affichage du mini_map et du mini_hero 
+*@param pointeur sur mini map et surface pour l'afficher sur elle
+*@return rien
+*/
 void afficher (mini_map m, SDL_Surface * screen)
 {
 	SDL_BlitSurface(m.background,NULL,screen,&m.posbackground);
 	SDL_BlitSurface(m.mini_personne,NULL,screen,&m.posmini_perosnne);
 }
-//4 Afficher temps depuis lancement du jeu et temps restant pour trouver une piece 
+/**
+*@brief Calculer le temps depuis lancement du jeu  et l'afficher 
+*@param variable qui possede le temps ecoule,fenetre 
+*@return rien
+*/
 void affichertemps(uint32_t start_time,SDL_Surface *screen)
 {
 	uint32_t time;
 	time=SDL_GetTicks() - start_time;
 	SDL_Rect position;
-	position.x=0;
-	position.y=0;
+	position.x=720;
+	position.y=-20;
 	position.w=0;
 	position.h=0;
-	int seconds,milleseconds;
+	int seconds,milleseconds,minutes=0;
 	seconds=time/1000;
+	minutes=seconds/60;
+	seconds-=minutes*60;
 	milleseconds=time%1000;
 	char text[1000];
 	TTF_Font *police;
-	police=TTF_OpenFont("/home/aziz/futur.ttf",15);
+	police=TTF_OpenFont("Pixeled.ttf",25);
 	SDL_Color Couleur;
 	Couleur.r=100;
 	Couleur.g=150;
 	Couleur.b=200;
-	sprintf(text,"Temps depuis lancement du jeu est :%u.%03u secondes",seconds,milleseconds);
+	sprintf(text,"TIME= %u:%u:%02u",minutes,seconds,milleseconds);
 	SDL_Surface *surface;
 	surface=TTF_RenderText_Solid(police,text,Couleur);
 	SDL_BlitSurface(surface,NULL,screen,&position);
 	TTF_CloseFont(police);
 	SDL_FreeSurface(surface);
 }
-//3 Mise a jour la position du captain dans la map 
-void MAJMinimap(SDL_Rect posJoueur,  mini_map * m, SDL_Rect camera, int redimensionnement)
+/**
+*@brief Moad animation du niveau 1 
+*@param pointeur sur mini_map 
+*@return rien
+*/
+void init_animation1(mini_map* i)
 {
-	m->posmini_perosnne.x=450+(posJoueur.x/redimensionnement) - m->mini_personne->w;
-	m->posmini_perosnne.y=100+(posJoueur.y/redimensionnement) - m->mini_personne->h;
-	/*SDL_Rect posJoueurAbsolue;
-	posJoueurAbsolue.x=posJoueur.x+camera.x;
-	posJoueurAbsolue.y=posJoueur.y+camera.y;	
-	m->posmini_perosnne.x=posJoueurAbsolue.x *(redimensionnement/100);
-	m->posmini_perosnne.y=posJoueurAbsolue.y *(redimensionnement/100);
-	m->posmini_perosnne.w=posJoueurAbsolue.x *(redimensionnement/100);
-	m->posmini_perosnne.h=posJoueurAbsolue.x *(redimensionnement/100);*/	
+	i->nb_img=10;
+	FILE* fich;
+	fich=fopen("data1.txt","r");
+    	i->img_actuelle=0;
+    	char nameIMG[30]; 
+    //parcoure
+    int ip;
+   for ( ip = 0; ip < 4; ip++)
+    {
+    
+        fscanf(fich,"%s",nameIMG);
+        i->img[ip] = IMG_Load(nameIMG);
+    }
+i->pos.x=1250;
+i->pos.y=0;
+fclose(fich);
 }
-//5 Afficher point couloré ou signe d'une piece du spaceship dans le minima
-/*void animerMinimap(mini_map *m)
+/**
+*@brief Moad animation du niveau 2 et niveau 3 
+*@param pointeur sur mini_map 
+*@return rien
+*/
+void init_animation2(mini_map* i)
 {
-	
-}*/
+	FILE* fich;
+	fich=fopen("data2.txt","r");
+    	i->img_actuelle=0;
+    	char nameIMG[30]; 
+    //parcoure
+    int ip;
+   for ( ip = 0; ip < 4; ip++)
+    {
+    
+        fscanf(fich,"%s",nameIMG);
+        i->img[ip] = IMG_Load(nameIMG);
+    }
+i->pos.x=1200;
+i->pos.y=25;
+fclose(fich);
+}
+/**
+*@brief Afficher  le sens de fin du chaque  niveau 
+*@param pointeur sur mini_map et fenetre pour afficher l'animation
+*@return rien
+*/
+void affiche_animation(mini_map * i,SDL_Surface *screen){
+ 	SDL_BlitSurface(i->img[i->img_actuelle],NULL,screen,&i->pos);
+    	i->img_actuelle++;
+    	SDL_Delay(400);
+    	if (i->img_actuelle == 4) i->img_actuelle=0;
+}
+/**
+*@brief Load masque du niveau 1
+*@param Surface
+*@return rien
+*/
 void initMasquelevel1(SDL_Surface *Masque)
 {
 	Masque=IMG_Load("/home/aziz/masque_level1.png");
 }
+/**
+*@brief Load masque du niveau 2
+*@param Surface
+*@return rien
+*/
 void initMasquelevel2(SDL_Surface *Masque)
 {
 	Masque=IMG_Load("/home/aziz/masque_level2.png");
 }
+/**
+*@brief Load masque du niveau 3
+*@param Surface
+*@return rien
+*/
 void initMasquelevel3(SDL_Surface *Masque)
 {
 	Masque=IMG_Load("/home/aziz/masque_level23.png");
 }
+/**
+*@brief Detecter si il y'a intersction les cordonnées du surface masqué entre parametre est le point du cordonnées (x,y) par 
+*@param Surface et coordonées de reference 
+*@return Color du masque du surface 
+*/
 int collisionPP(Personne P,SDL_Surface *Masque)
 {
 	int collision=0,i;
@@ -143,6 +250,11 @@ int collisionPP(Personne P,SDL_Surface *Masque)
 		collision=1;}
 	return collision;
 }
+/**
+*@brief Detecter si il y'a intersction les cordonnées du surface masqué entre parametre est le point du cordonnées (x,y) par 
+*@param Surface et coordonées de reference 
+*@return Color du masque du surface 
+*/
 SDL_Color GetPixel(SDL_Surface *pSurface,int x,int y)
 {
 	SDL_Color color;
@@ -154,9 +266,14 @@ SDL_Color GetPixel(SDL_Surface *pSurface,int x,int y)
 	SDL_GetRGB(col,pSurface->format,&color.r,&color.g,&color.b);
 	return color;
 }
-//6
+/**
+*@brief Liberer mémoire les resources
+*@param  pointeur sur mini_map
+*@return rien
+*/
 void Liberer (mini_map  *m)
 {
 	SDL_FreeSurface(m->background);
 	SDL_FreeSurface(m->mini_personne);
+	SDL_FreeSurface(m->animation);
 }
--- mini_map.h
+++ mini_map.h
@@ -5,11 +5,20 @@
 #include<SDL/SDL.h>
 #include<SDL/SDL_image.h>
 #include<SDL/SDL_mixer.h>
+/** 
+*@struct Background
+*@brief structure du background du niveaux
+*/
 typedef struct Image
 {
-	SDL_Rect pos1;
-	SDL_Surface *image;
+	SDL_Rect pos1;/*!<position du background*/
+	SDL_Surface *image;/*!<Surface du background*/
+	
 }image;
+/** 
+*@struct text
+*@brief structure du text
+*/
 typedef struct text2
 {
 	SDL_Rect pos3;
@@ -17,41 +26,34 @@ typedef struct text2
 	SDL_Surface *surface_text;
 	SDL_Color couleur;
 } text;
+/** 
+*@struct mini_map
+*@brief structure de minimap
+*/
 typedef struct mini_map
 {
-	SDL_Surface *background,*mini_personne;
-	SDL_Rect posmini_perosnne,posbackground;
+	SDL_Surface *background,*mini_personne,*animation;/*!<Surface du mini_map et du minihero et d'animation*/
+	SDL_Rect posmini_perosnne,posbackground;/*!<position du minihero et position du mini map*/
+	SDL_Rect pos;/*!<position d'animation*/
+	SDL_Surface *img[4];/*!<Tableu d'image*/
+	int img_actuelle;/*!<numero d'image actuelle*/
 }mini_map;
-typedef struct Personne{
-	SDL_Surface *perso;
-	SDL_Rect PosEcran;
-	int Direction,vie,score,vitesse_saut,sprite;
-	double vitesse, acceleration;
-	Mix_Chunk *bref;
-}Personne;
-//Mini_map
 void level_1(image *i);
 void level_2(image *i);
 void level_3(image *i);
-void Rezise(image i,mini_map *m);
 void initmap_level1(mini_map *m);
 void initmap_level2(mini_map *m);
 void initmap_level3(mini_map *m);
 void afficher (mini_map m, SDL_Surface * screen);
 Uint32  calculertemps(Uint32 start_time);
 void affichertemps(Uint32 time,SDL_Surface *screen);
-void MAJMinimap(SDL_Rect posJoueur,  mini_map * m, SDL_Rect camera, int redimensionnement);
-void animerMinimap(mini_map *m);
+void init_animation1(mini_map* i);
+void init_animation2(mini_map* i);
+void affiche_animation(mini_map * i,SDL_Surface *screen);
 void initMasquelevel1(SDL_Surface *Masque);
 void initMasquelevel2(SDL_Surface *Masque);
 void initMasquelevel3(SDL_Surface *Masque);
 SDL_Color GetPixel(SDL_Surface *pSurface,int x,int y);
 int collisionPP(Personne P,SDL_Surface *Masque);
 void Liberer (mini_map  *m);
-//Personne
-void initPerso(Personne *p);
-void afficherPerso(Personne p, SDL_Surface * screen);
-void movePerso (Personne *p, uint32_t dt);
-void animerPerso (Personne* p);
-void saut (Personne* p);
 #endif 
